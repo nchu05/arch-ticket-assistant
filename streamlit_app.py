dec707feb835db6cbdd2b45920dfe6c1bd856ee9
Nathan Chu <122313343+nchu05@users.noreply.github.com>
2024-07-30 20:53:52 +0000

    Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,170 @@
 import streamlit as st
 from openai import OpenAI
+import requests
+from datetime import timedelta, datetime
+import pandas as pd
+import json
+import time
 
 # Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+st.title("ARCH Ticket Assistant")
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+openai_api_key = st.secrets.openai_api_key
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+client = OpenAI(api_key=openai_api_key)
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+def get_ticket_details(start_date, end_date):
+  url = st.secrets.token_url
+  payload = {
+      "grant_type": "client_credentials",
+      "client_id": st.secrets.client_id,
+      "client_secret": st.secrets.client_secret,
+  }
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+  response = requests.post(url, data=payload)
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+  access_token = response.json().get('access_token')
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+  headers = {
+          'Authorization': 'Bearer:' + access_token,
+          'Content-Type': 'application/json'
+      }
+  if not start_date or datetime.strptime(start_date, "%Y-%m-%d").date() > datetime.now().date():
+      start_date = (datetime.now().date() - timedelta(days=7)).strftime('%Y-%m-%d')
+  if not end_date or datetime.strptime(end_date, "%Y-%m-%d").date() > datetime.now().date():
+      end_date=(datetime.now().date()).strftime('%Y-%m-%d')
+  print(start_date)
+  print(end_date)
+  response = requests.get('https://uat-archapi.niagarawater.com/api/tickets/getAllTicketDetails?usecase_id=[]&start_date={}&end_date={}'.format(start_date, end_date), headers = headers)
+  json = response.json()
+  df = pd.DataFrame(json['response'])
+  print(df)
+  try:
+    df = df.drop('_id', axis=1)
+    df = df.drop('lastest_Message', axis=1)
+  except:
+    pass
+  df.sort_values(by='ticket_creation_date', ascending=False, inplace=True)
+  return df.to_json(orient='index')
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
+class AssistantManager:
+    assistant_id = st.secrets.assistant_id
+
+    def __init__(self):
+        self.client = client
+        self.model = "gpt-3.5-turbo"
+        self.thread = None
+        self.run = None
+
+        self.assistant = self.client.beta.assistants.retrieve(
+            assistant_id=AssistantManager.assistant_id
         )
+    
+    
+    def create_thread(self):
+        if not self.thread:
+            thread_obj = self.client.beta.threads.create()
+            self.thread = thread_obj
+            print(f"THREAD ID: {self.thread.id}")
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+  
+    def add_message_to_thread(self, role, content):
+        if self.thread:
+            self.client.beta.threads.messages.create(
+                thread_id=self.thread.id, role=role, content=content
+            )
+
+    def run_assistant(self, instructions):
+        if self.thread and self.assistant:
+            self.run = self.client.beta.threads.runs.create(
+                thread_id=self.thread.id,
+                assistant_id=self.assistant.id,
+                instructions=instructions
+            )
+    
+    def process_message(self):
+        if self.thread:
+            messages = self.client.beta.threads.messages.list(thread_id=self.thread.id)
+            summary = []
+
+            last_message = messages.data[0]
+            role = last_message.role
+            response = last_message.content[0].text.value
+            summary.append(response)
+
+            self.summary = "\n".join(summary)
+            return response
+
+    def call_required_functions(self, required_actions):
+        if not self.run:
+            return
+        tool_outputs = []
+        for action in required_actions["tool_calls"]:
+            func_name = action["function"]["name"]
+            arguments = json.loads(action['function']['arguments'])
+
+            if func_name == "get_ticket_details":
+                output = get_ticket_details(start_date=arguments.get("start_date"), end_date=arguments.get("end_date"))
+                tool_outputs.append({"tool_call_id" : action["id"], "output" : output})
+            else:
+                raise ValueError(f"Unknown function: {func_name}")
+        
+        self.client.beta.threads.runs.submit_tool_outputs(
+            thread_id=self.thread.id, run_id=self.run.id, tool_outputs=tool_outputs
+        )
+      
+    def wait_for_completion(self):
+        if self.thread and self.run:
+            while True:
+                time.sleep(1)
+                run_status = self.client.beta.threads.runs.retrieve(
+                    thread_id=self.thread.id, run_id=self.run.id
+                )
+
+                if run_status.status == "completed":
+                    return self.process_message()
+                elif run_status.status == "requires_action":
+                    self.call_required_functions(
+                        required_actions=run_status.required_action.submit_tool_outputs.model_dump()
+                    )
+                elif run_status.status == "failed":
+                    break
+
+    def run_steps(self):
+        run_steps = self.client.beta.threads.runs.steps.list(
+            thread_id=self.thread.id, run_id=self.run.id
+        )
+        print(f"Run-Steps::: {run_steps}")
+        return run_steps.data
+
+# Create a session state variable to store the chat messages. This ensures that the
+# messages persist across reruns.
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display the existing chat messages via `st.chat_message`.
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+manager = AssistantManager()
+manager.create_thread()
+
+# Create a chat input field to allow the user to enter a message. This will display
+# automatically at the bottom of the page.
+if prompt := st.chat_input("Message ARCH API Assistant"):
+
+    # Store and display the current prompt.
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    manager.add_message_to_thread(role="user", content=f"Answer the question as follows: {prompt}")
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    manager.run_assistant(instructions="Answer the question about maintenance tickets.")
+    response = manager.wait_for_completion()
+    print(response)
+
+    # Stream the response to the chat using `st.write_stream`, then store it in 
+    # session state.
+    with st.chat_message("assistant"):
+        st.write(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
